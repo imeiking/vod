cb50c5f8545dbcb16b010bec9501dc02eb38c834
imeiking <haohaoxuexi1@protonmail.com>
2025-11-08 23:01:13 +0800

    Create agedm.js

---
+++ agedm.js
@@ -0,0 +1,6 @@
+{ 
+            "name": "AGE动漫", 
+            "type": 3, 
+            "api": "csp_age",
+            "ext": "https://raw.githubusercontent.com/imeiking/js/refs/heads/main/agedm.js"
+        }
