f0b21cae053580da8c5f8f605c460d2552098759
imeiking <haohaoxuexi1@protonmail.com>
2026-06-14 17:34:32 +0800

    Delete agedm.js

--- agedm.js
+++
@@ -1,6 +0,0 @@
-{ 
-            "name": "AGE动漫", 
-            "type": 3, 
-            "api": "csp_age",
-            "ext": "https://raw.githubusercontent.com/imeiking/js/refs/heads/main/agedm.js"
-        }
